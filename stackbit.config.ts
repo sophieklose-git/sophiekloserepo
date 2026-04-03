498bd273f89b35ebfb658193625022434ccdd56f
sophieklose-git <sophie.klose@googlemail.com>
2026-04-03 20:05:54 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,7 +1,7 @@
-// Example snippet for stackbit.config.ts
+// Example snippet for stackbit.config.ts  
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
   ssgName: 'static', // Tells Netlify this is a static HTML site
   devCommand: 'npx serve . -p {port}', // The command to start your preview
   // ... rest of your config
-});
+});   
